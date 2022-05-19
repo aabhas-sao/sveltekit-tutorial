e8f5c95551277818e932b494d81cd9e95763a18d
Aabhas Sao <aabhassao0@gmail.com>
2022-05-19 16:47:27 +0530

    chore: initial commit
    Signed-off-by: Aabhas Sao <aabhassao0@gmail.com>

---
+++ src/routes/about.svelte
@@ -0,0 +1,18 @@
+<div class="about">
+  <h1>About Page</h1>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit,
+    consequuntur quo. Magnam ipsa maiores quaerat ullam, provident neque
+    perferendis pariatur. Cupiditate ullam repellat dignissimos! Atque
+    laboriosam nam voluptatem veniam doloremque.
+  </p>
+</div>
+
+<style>
+  .about {
+    font-family: sans-serif;
+    width: 50vw;
+    margin: auto;
+    line-height: 1.8;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  let title = "Ninja Gaming Guides";
+
+  const updateTitle = () => {
+    title = "Demon Slayer Guides";
+  };
+</script>
+
+<h1>{title}</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<button on:click={updateTitle}>change title</button>
+<input type="text" name="title" bind:value={title} />
